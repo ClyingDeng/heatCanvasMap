5d06de7face0100f0a9965651250b526ec5de804
dengying <1174915260@qq.com>
2024-04-27 18:15:13 +0800

    feat: 修改

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,14 +12,34 @@ export default {
       mapBounds: null,
       latlngs: [
         {
-          lng: 116.404,
+          lng: 116.46754429734781,
+          lat: 39.9610120606158,
+          count: 67.08430718281981
+        },
+        {
+          lng: 116.45388932327177,
+          lat: 39.93922650653802,
+          count: 87.27049672583702
+        },
+        {
+          lng: 116.51676961427235,
+          lat: 39.957355396010655,
+          count: 65.19401496400552
+        },
+        {
+          lng: 116.50268151440899,
+          lat: 40.00267237928021,
+          count: 49.24838394729958
+        },
+        {
+          lng: 115.404,
           lat: 39.915,
           count: 80
         },
         {
-          lng: 117.3348329641738,
-          lat: 39.262690333,
-          count: 30
+          lng: 115.4148329641738,
+          lat: 39.913690333,
+          count: 90
         }
       ],
       map: null,
@@ -29,61 +49,18 @@ export default {
       radius: 20 // 圆点
     }
   },
-  computed: {
-    // pointsBounds() {
-    //   let mapData = []
-    //   let r = this.map.getBounds()
-    //   console.log(r)
-    //   if (r.equals(this.mapBounds)) return
-    //   this.mapBounds = r
-    //   let n = this.latlngs.length
-    //   while (n--) {
-    //     let f = this.latlngs[n]
-    //     if (!r.containsPoint(f)) {
-    //       continue
-    //     } // 超出可是范围的不画
-    //     let point = this.map.pointToOverlayPixel({
-    //       lng: this.latlngs[n].lng,
-    //       lat: this.latlngs[n].lat,
-    //       radius: this.radius
-    //     })
-    //     // 需要渲染的点
-    //     mapData.push({ lng: point.x, lat: point.y, count: this.latlngs[n].count })
-    //   }
-    //   return mapData
-    // }
-  },
-  watch: {
-    // 监听地图变化
-    // mapWatch: {
-    //   handler() {
-    //     if (!this.map) return
-    //     let mapData = []
-    //     let r = this.map.getBounds()
-    //     console.log(r)
-    //     if (r.equals(this.mapBounds)) return
-    //     this.mapBounds = r
-    //     let n = this.latlngs.length
-    //     while (n--) {
-    //       let f = this.latlngs[n]
-    //       if (!r.containsPoint(f)) {
-    //         continue
-    //       } // 超出可是范围的不画
-    //       let point = this.map.pointToOverlayPixel({
-    //         lng: this.latlngs[n].lng,
-    //         lat: this.latlngs[n].lat,
-    //         radius: this.radius
-    //       })
-    //       // 需要渲染的点
-    //       mapData.push({ lng: point.x, lat: point.y, count: this.latlngs[n].count })
-    //     }
-    //     this.pointsBounds = mapData
-    //   },
-    //   immediate: true,
-    //   deep: true
-    // }
-  },
   mounted() {
+    this.latlngs = []
+    for (let i = 0; i < 300; i++) {
+      let randomLng = 116.404 + Math.random() * 0.5
+      let randomLat = 39.215 + Math.random() * 0.8
+      this.latlngs.push({
+        //地理坐标
+        lng: randomLng,
+        lat: randomLat,
+        count: Math.random() * 100
+      })
+    }
     this.map = new BMapGL.Map('container')
     let point = new BMapGL.Point(116.404, 39.915)
     this.map.centerAndZoom(point, 8)
@@ -99,38 +76,40 @@ export default {
       // 数据点中 y 坐标的属性名称
       valueField: 'count'
     })
-    this.pointsBounds()
-    // 设置热力图数据
-    this.heatmapCanvas.setData({
-      min: 0,
-      max: 100,
-      data: this.points
-    })
-    this.canvas = this.heatmapCanvas._render.canvas
-
-    const { sw, ne } = this.map.getBounds()
-    var bounds = new BMapGL.Bounds(
-      new BMapGL.Point(sw.lng, sw.lat),
-      new BMapGL.Point(ne.lng, ne.lat)
-    )
-    var canvasOverlay = new BMapGL.GroundOverlay(bounds, {
-      type: 'canvas',
-      url: this.canvas
-    })
-    this.map.addOverlay(canvasOverlay)
-
+    this.renderMap()
     // 监听地图缩放事件
     this.map.addEventListener('zoomend', () => {
-      this.pointsBounds()
+      this.renderMap()
     })
     // // 监听地图拖拽事件
     this.map.addEventListener('dragend', () => {
-      this.pointsBounds()
+      this.renderMap()
     })
   },
   methods: {
+    renderMap() {
+      this.pointsBounds() // 获取数据新位置
+      // 设置热力图数据
+      this.heatmapCanvas.setData({
+        min: 0,
+        max: 170,
+        data: this.points
+      })
+      this.canvas = this.heatmapCanvas._render.canvas
+      const { sw, ne } = this.map.getBounds()
+      var bounds = new BMapGL.Bounds(
+        new BMapGL.Point(sw.lng, sw.lat),
+        new BMapGL.Point(ne.lng, ne.lat)
+      )
+      var canvasOverlay = new BMapGL.GroundOverlay(bounds, {
+        type: 'canvas',
+        url: this.canvas
+      })
+      this.map.addOverlay(canvasOverlay)
+    },
     // 仅绘制区域内的点
     pointsBounds() {
+      this.map.clearOverlays()
       let mapData = []
       let r = this.map.getBounds()
       if (r.equals(this.mapBounds)) return
@@ -149,7 +128,6 @@ export default {
         // 需要渲染的点
         mapData.push({ lng: point.x, lat: point.y, count: this.latlngs[n].count })
       }
-      console.log(mapData)
       this.points = mapData
     }
   }
